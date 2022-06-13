0aeaffac0ea68a850266c4a6aa40b42bc7a534f1
Ranem97 <95642418+Ranem97@users.noreply.github.com>
2022-06-13 14:09:10 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="btn btn-primary">Hello</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
