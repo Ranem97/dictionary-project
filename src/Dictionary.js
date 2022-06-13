e5923fa56b349287d06e433e6d532c503ec17b0f
Ranem97 <95642418+Ranem97@users.noreply.github.com>
2022-06-13 19:59:58 +0300

    added the search api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
   function handleSubmit(event) {
     event.preventDefault();
     alert(`searching for ${keyword}`);
+
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
