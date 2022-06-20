d9745a01f74cc7859b8da191fda0d5c9859dcc3e
Ranem97 <95642418+Ranem97@users.noreply.github.com>
2022-06-20 15:39:14 +0300

    added phonetics text and audio

--- src/Resault.js
+++ src/Resault.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Resault(props) {
   if (props.resault) {
     return (
       <div className="Resault">
-        <h2>{props.resault.word}</h2>
+        <h1>{props.resault.word}</h1>
+        {props.resault.phonetics.map(function (phonetic, index) {
+          return <Phonetic phonetic={phonetic} />;
+        })}
         {props.resault.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
